f43c7748271aed86bec0f7fac16250428040eed8
Malitha Supun <malithasupun2@gmail.com>
2024-07-12 14:48:55 +0530

    add new assets folder and button.jsx coded

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-function App() {
+import ButtonGradient from "./assets/svg/ButtonGradient"
+import Button from "./components/Button";
+
+const App = () =>{
 
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+    <Button className="mt-10">something</Button>
+
+    </div>
+    <ButtonGradient />
+    </>
   );
 }
 
