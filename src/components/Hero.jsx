03ef92008e9a9824f1fe4a7b5824b90c19b9a776
Malitha Supun <malithasupun2@gmail.com>
2024-07-18 11:48:18 +0530

    Added curve image to Hero component's h1 tag

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,12 @@
 import Section from './Section';
+import curve from "../assets/hero/curve.png";
 
 const Hero = () => {
   return (
     <Section className="pt-[12rem] -mt-[5.25]" crosses crossesOffset="lg:translate-y-[5.25rem]" customPaddings id="hero">
     <div className="container relative">
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb:[6rem]">
-            <h1 className="h1 mb-6">Explore the Possibilites of AI Chatting with BrainwaveCurve
+            <h1 className="h1 mb-6">Explore the Possibilites of AI Chatting with BrainwaveCurve<span className='inline-block relative'>Brainwave <img src={curve} /></span>
             </h1>
         </div>
     </div>
