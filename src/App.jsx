c32c89da57b3b6dc4fb925c85c93312d03f19f26
Malitha Supun <malithasupun2@gmail.com>
2024-07-18 11:38:28 +0530

    added Hero import, and added Hero component to App JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import ButtonGradient from "./assets/svg/ButtonGradient"
-import Button from "./components/Button";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const App = () =>{
 
@@ -11,6 +11,7 @@ const App = () =>{
     <Header />
     </div>
     <ButtonGradient />
+    <Hero />
     </>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import Section from './Section';
+
+const Hero = () => {
+  return (
+    <Section className="pt-[12rem] -mt-[5.25]" crosses crossesOffset="lg:translate-y-[5.25rem]" customPaddings id="hero">
+    <div className="container relative">
+        <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb:[6rem]">
+            <h1 className="h1 mb-6">Explore the Possibilites of AI Chatting with BrainwaveCurve
+            </h1>
+        </div>
+    </div>
+    </Section>
+  );
+};
+
+export default Hero
