657fd3fd5cde6a35d14f0b7ec68b5f695b474bfe
Malitha Supun <malithasupun2@gmail.com>
2024-07-20 12:07:34 +0530

    Added react-just-parallax dependency and updated Hero component with parallax effects, new design elements, and refactored code structure.

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,10 @@ const App = () =>{
     <>
     <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
     <Header />
-    </div>
-    <ButtonGradient />
     <Hero />
+    </div>
+    
+    <ButtonGradient />
     </>
   );
 }
