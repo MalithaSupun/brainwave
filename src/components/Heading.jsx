47606d4b073748c2ded6d1f3f404c4b08f554ce8
Malitha Supun <malithasupun2@gmail.com>
2024-07-27 00:43:04 +0530

    Updated Heading component to accept 'tag' prop and added Tagline component.

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,6 +1,11 @@
-const Heading = ({className, title, text}) => {
+import Tagline from "./Tagline";
+
+const Heading = ({className, title, text, tag}) => {
   return (
     <div className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20`}>
+    {tag && (
+      <Tagline></Tagline>
+    )}
     {title && <h2 className="h2">{title}</h2>}
     {text && <p className="body-2 mt-4 text-n-4">{text}</p>}
     </div>
---
+++ src/components/Tagline.jsx
@@ -0,0 +1,9 @@
+const Tagline = () => {
+  return (
+    <div>Tagline</div>
+  )
+}
+
+export default Tagline
+
+//2.36 stoped
